debb027e3f4e8cfea115fa0109d09cdaa8bd9a5b
daxterstroll <daxterstroll@gmail.com>
2018-05-17 01:10:44 +0300

    Also you cannot leave a review for an item you never booked.

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,8 +7,10 @@ class Review < ActiveRecord::Base
 
   validate :must_be_booking_item
   def must_be_booking_item
-    test = Booking.find_by user_id: reviewable_id
-    unless reviewable_id == test.user_id
+    test1 = Booking.find_by item_id: reviewable_id
+    test2 = Booking.find_by user_id: user_id
+
+    unless (reviewable_id == test1.item_id) && (user_id == test2.user_id)
       errors[:base] << 'Must be booking to leave a review'
     end
   end
