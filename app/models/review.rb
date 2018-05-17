c499c61393861354df696617baf4bf6c5697c1d9
daxterstroll <daxterstroll@gmail.com>
2018-05-17 14:31:12 +0300

    update validates for review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,14 +5,12 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
 
-  validate :must_be_booking_item
-  def must_be_booking_item
-    test1 = Booking.find_by item_id: reviewable_id
-    test2 = Booking.find_by user_id: user_id
+  validates :reviewable_id, presence: true
+  validates :reviewable_type, presence: true
 
-    unless (reviewable_id == test1.item_id) && (user_id == test2.user_id)
-      errors[:base] << 'Must be booking to leave a review'
-    end
+  validate :must_be_booking
+
+  def must_be_booking
+    errors.add(:reviewable, 'Must be booking to leave a review') unless reviewable == Booking.find_by(item_id: reviewable_id)
   end
-
 end
