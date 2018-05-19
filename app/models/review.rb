1674271ac4f2c9d0ba5818eaca994b4364d26d57
daxterstroll <daxterstroll@gmail.com>
2018-05-19 19:10:12 +0300

    Also you cannot leave a review for an item you never booked.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,14 @@ class Review < ActiveRecord::Base
 
 #Подключает полиморфизм
   belongs_to :reviewable, :polymorphic => true
+
+  validates :reviewable_id, presence: true
+  validates :reviewable_type, presence: true
+  validate :must_be_booking
+
+  def must_be_booking
+    errors.add(:reviewable, 'You must rent a item to leave a review') unless
+        Booking.exists?(user_id: user_id, item_id: reviewable_id) == true
+  end
+
 end
