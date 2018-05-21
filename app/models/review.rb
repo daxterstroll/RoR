edbc280d934599089b93d81d26ba171eae1330de
daxterstroll <daxterstroll@gmail.com>
2018-05-21 16:31:05 +0300

    add validate for user review

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,9 +6,16 @@ class Review < ActiveRecord::Base
   # checking on booking
   validate :must_be_booking
   def must_be_booking
-    bool = User.joins(bookings: :item)
-               .exists?(users: { id: user_id },
-                        bookings: { item_id: reviewable_id })
-    bool == true ? return : errors.add(:reviewable, 'You have not booked')
+    if reviewable_type == 'Item'
+      bool = User.joins(bookings: :item)
+                 .exists?(users: { id: user_id },
+                          bookings: { item_id: reviewable_id })
+      bool == true ? return : errors.add(:reviewable, 'You have not booked')
+    elsif reviewable_type == 'User'
+      bool = User.joins(bookings: :item)
+                 .exists?(users: { id: user_id},
+                          items: { user_id: reviewable_id})
+      bool == true ? return : errors.add(:reviewable, 'You are not trading')
+    end
   end
 end
