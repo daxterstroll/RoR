01fbf6d8e8716f7fdce06c32f8a3f1aea16e0a29
daxterstroll <daxterstroll@gmail.com>
2018-05-21 18:17:25 +0300

    testing select for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+City.create([{ name: 'Odessa' },
+             { name: 'Simferopol' }])
+
+User.create([{ name: 'Oksana', city_id: City.find_by(name: 'Simferopol').id },
+             { name: 'Diana', city_id: City.find_by(name: 'Odessa').id }])
+
+Item.create([{ name: 'coca-cola', user_id: User.find_by(name: 'Oksana').id },
+             { name: 'boots', user_id: User.find_by(name: 'Diana').id }])
+
+Booking.create([{ started_at: '2018-05-21',
+                  ended_on: '2018-05-25',
+                  item_id: Item.find_by(name: 'coca-cola',
+                                        user_id: User.find_by(name: 'Oksana')).id,
+                  user_id: User.find_by(name: 'Diana').id }])
+
+Booking.create([{ started_at: '2018-05-22',
+                  ended_on: '2018-05-26',
+                  item_id: Item.find_by(name: 'boots',
+                                        user_id: User.find_by(name: 'Diana')).id,
+                  user_id: User.find_by(name: 'Oksana').id }])
+
+Review.create([{ reviewable_id: Item.find_by(name: 'coca-cola').id,
+                 reviewable_type: 'Item',
+                 user_id: User.find_by(name: 'Diana').id,
+                 message: 'Good drink' }])
+
+Review.create([{ reviewable_id: Item.find_by(name: 'boots').id,
+                 reviewable_type: 'Item',
+                 user_id: User.find_by(name: 'Oksana').id,
+                 message: 'Nice boots!' }])
