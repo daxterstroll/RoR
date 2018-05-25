586d4b33f33bd059a21d157ace47ed8077056906
daxterstroll <daxterstroll@gmail.com>
2018-05-26 00:18:13 +0300

    for Postman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # CSRF
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
   def index
     @title = 'About Us'
   end
